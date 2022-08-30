22afee2bd943f30c0773705ffd7be1d6c184f936
Olive <olivelliott48@gmail.com>
2022-08-29 21:42:10 -0400

    Add login and addUser mutations and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -31,7 +31,12 @@ const typeDefs = gql`
         user(username: String!): User
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
-    }    
+    } 
+    
+    type Mutation {
+        login(email: String!, password: String!): User
+        addUser(username: String!, email: String!, password: String!): User
+    }
 `;
 
 module.exports = typeDefs;
