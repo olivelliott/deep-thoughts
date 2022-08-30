e2428f6197c18f52149150b1747267bdf6c2a006
Olive <olivelliott48@gmail.com>
2022-08-29 21:26:42 -0400

    Build remaining Query types and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,6 +5,24 @@ const resolvers = {
         thoughts: async (parent, { username }) => {
             const params = username ? { username } : {};
             return Thought.find(params).sort({ createdAt: -1 });
+        },
+
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
+        },
+
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts')
         }
     }
 };
