dd0a9986de1970350c7492c70f2ce70180c8e577
Olive <olivelliott48@gmail.com>
2022-09-04 21:42:51 -0400

    Create and implement authentication functionality

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
 
 const Signup = () => {
   const [formState, setFormState] = useState({
@@ -27,7 +28,8 @@ const Signup = () => {
       const { data } = await addUser({
         variables: { ...formState },
       });
-      console.log(data);
+
+      Auth.login(data.addUser.token);
     } catch (e) {
       console.error(e);
     }
